44a8ecb8a0b8a64aba4a134871131b9341ed0539
VisualOption <Gunmetal90@gmail.com>
2018-04-22 17:02:45 +0300

    Second test commit

--- src/main/java/condition/prototype.java
+++ src/main/java/condition/prototype.java
@@ -5,5 +5,6 @@ public class prototype {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
