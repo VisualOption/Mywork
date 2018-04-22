ac73f7596f16205f9546c744a5fd2a6857f7f871
VisualOption <Gunmetal90@gmail.com>
2018-04-22 16:20:11 +0300

    Next test commit

--- src/main/java/condition/prototype.java
+++ src/main/java/condition/prototype.java
@@ -1,4 +1,9 @@
 package condition;
 
 public class prototype {
+    public static void main (String [] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
 }
